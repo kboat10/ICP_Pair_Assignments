7de444ea24fdc587da1243d33122f5ee561bbf48
Aseda-Bioh <biohaseda07@gmail.com>
2024-10-12 12:14:32 +0000

    some new changes

--- Account.cpp
+++ Account.cpp
@@ -1,8 +1,34 @@
 #include<iostream>
 #include "Account.h"
-#ifndef ACCOUNT
-#define ACCOUNT
 using namespace std;
 
+Account::Account(int accountNumber, string firstName, string lastName, string dob, string gender, string address, int phoneContact, string email, double balance): Customer(accountNumber, firstName, lastName, dob, gender, address, phoneContact, email) {
+    if (balance >= 0.0)
+    {
+        this -> balance = balance;
+    }
+    
+    else {
+        balance = 0.0;
+        cout << "Invalid balance entered.";
+    }
+}
 
-#endif
+void Account::credit(double amount) {
+    balance += amount;
+}
+
+void Account::debit(double amount) {
+    if (amount > balance)
+    {
+        cout << "You do not have enough funds for this transaction.";
+    }
+
+    else {
+        balance -= amount;
+    }
+}
+
+double Account::getBalance() {
+    return balance;
+}
